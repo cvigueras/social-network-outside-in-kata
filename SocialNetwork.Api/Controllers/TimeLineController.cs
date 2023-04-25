b6d747e4237bd9c2299e132080477f7b0b55cd44
cvigueras <carlosj.vigueras@gmail.com>
2023-04-25 10:42:54 +0200

    Test: GetEmptyWhenGetOwnMessages

---
+++ SocialNetwork.Api/Controllers/TimeLineController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetwork.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TimeLineController : ControllerBase
+{
+    [HttpGet("{user}")]
+    public object Get(string user)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ SocialNetwork.Test/TimeLineControllerShould.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using SocialNetwork.Api.Controllers;
+
+namespace SocialNetwork.Test
+{
+    public class TimeLineControllerShould
+    {
+        [Test]
+        public async Task GetEmptyWhenGetOwnMessages()
+        {
+            var timelineController = new TimeLineController();
+
+            var result = timelineController.Get("Alice");
+
+            result.ToString().Should().BeEmpty();
+        }
+    }
+}
