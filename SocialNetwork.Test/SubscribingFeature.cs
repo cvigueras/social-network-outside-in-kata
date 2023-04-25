ed50c498bfa5350f394eb5de0322a141f30fd602
cvigueras <carlosj.vigueras@gmail.com>
2023-04-25 16:34:07 +0200

    Test: GetOwnUserAndSubscribingMessagesAfterOtherUsersPost.

---
+++ SocialNetwork.Test/SubscribingFeature.cs
@@ -0,0 +1,47 @@
+﻿using NSubstitute;
+using SocialNetwork.Api;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace SocialNetwork.Test
+{
+    public class SubscribingFeature
+    {
+        [Test]
+        public async Task GetOwnUserAndSubscribingMessagesAfterOtherUsersPost()
+        {
+            var time = Substitute.For<ITime>();
+            var socialNetwork = new SocialNetworkApplication(time);
+            time.Timestamp().Returns(new DateTime(2023, 4, 18, 14, 35, 0),
+                new DateTime(2023, 4, 18, 14, 38, 0),
+                new DateTime(2023, 4, 18, 14, 48, 0));
+
+            var client = socialNetwork.CreateClient();
+
+            var result = await client.PostAsync("/Timeline/Bob", 
+                new StringContent("{\"post\":\"Hello I am Bob\"}", Encoding.Default, "application/json"));
+            result.EnsureSuccessStatusCode();
+            result = await client.PostAsync("/Timeline/Alice", 
+                new StringContent("{\"post\":\"Hello I am Alice\"}", Encoding.Default, "application/json"));
+            result.EnsureSuccessStatusCode();
+            result = await client.PostAsync("/Timeline/Charlie", 
+                new StringContent("{\"post\":\"Hello I am Charlie\"}", Encoding.Default, "application/json"));
+            result.EnsureSuccessStatusCode();
+
+            result = await client.PostAsync("/Subscribing/Alice",
+                new StringContent("{\"user\":\"Charlie\"}", Encoding.Default, "application/json"));
+            result.EnsureSuccessStatusCode();
+            result = await client.PostAsync("/Subscribing/Bob",
+                new StringContent("{\"user\":\"Charlie\"}", Encoding.Default, "application/json"));
+            result.EnsureSuccessStatusCode();
+
+            var response = await client.GetAsync("/Wall/Charlie");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var json = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(content), Formatting.Indented);
+
+            await Verify(json);
+        }
+    }
+}
