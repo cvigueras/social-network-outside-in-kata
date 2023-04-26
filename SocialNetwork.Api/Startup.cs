af2141e3cb4cb8c5c37605724ef35ff5ada1c383
cvigueras <carlosj.vigueras@gmail.com>
2023-04-26 10:35:45 +0200

    Feat: Implement dependency injection in startup class for Subscription Repository.

--- SocialNetwork.Api/Startup.cs
+++ SocialNetwork.Api/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data.SQLite;
 using SocialNetwork.Api.Data;
 using SocialNetwork.Api.Messages;
+using SocialNetwork.Api.Subscriptions;
 using SocialNetwork.Api.Time;
 
 namespace SocialNetwork.Api
@@ -28,6 +29,7 @@ namespace SocialNetwork.Api
 
             services.AddScoped(_ => new SQLiteConnection("Data Source=./SocialNetwork.db"));
             services.AddScoped<IMessagesRepository, MessageRepository>();
+            services.AddScoped<ISubscriptionRepository, SubscriptionRepository>();
             services.AddSingleton<ITime, Time.Time>();
         }
 
